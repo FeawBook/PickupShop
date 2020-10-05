7ee27917b563260d3e9a714257f73f698f693754
Thanathip <feaw.book@gmail.com>
2020-10-05 15:01:12 +0700

    [add][success] add table view and cell

---
+++ ShopPickup/Views/Cells/PickupCell.swift
@@ -0,0 +1,23 @@
+//
+//  PickupCell.swift
+//  ShopPickup
+//
+//  Created by Thanathip on 5/10/2563 BE.
+//
+
+import UIKit
+
+class PickupCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ShopPickup/Views/PickupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PickupViewController.swift
+//  ShopPickup
+//
+//  Created by Thanathip on 5/10/2563 BE.
+//
+
+import UIKit
+
+class PickupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
